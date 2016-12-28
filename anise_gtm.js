88748b2c9f91b0221c0be65661ee8ae648919d7b
Lex <stwo86@hotmail.com>
2016-12-28 22:41:22 +0800

    Add files via upload

--- anise_gtm.js
+++ anise_gtm.js
@@ -3,5 +3,5 @@
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-D8SG');
+})(window,document,'script','dataLayer','GTM-PGFW389');
 <!-- End Google Tag Manager -->
