f5cf8ea73bfcbfab36122d1c1d036f8ccfabea01
Lex <stwo86@hotmail.com>
2023-10-09 12:53:40 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 $(function() {
-	$("li.item a").click(function () {
-		var _Gotab_id = $(this).attr("title");
-		//alert($(_Gotab_id).offset())
-        $("html,body").animate({scrollTop:($(_Gotab_id).offset().top)-55}, 500);
-		
-		//
-	});
+    $("li.item a").click(function() {
+        var _Gotab_id = $(this).attr("title");
+        //alert($(_Gotab_id).offset())
+        $("html,body").animate({ scrollTop: ($(_Gotab_id).offset().top) - 55 }, 500);
+
+        //
+    });
+    //
+    $(window).scrollTop() < 200 ? $(".gotop").fadeOut(100) : $(".gotop").fadeIn(100);
 });
